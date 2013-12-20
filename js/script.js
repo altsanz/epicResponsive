e02552199209fe08f122acbad8489b5440c05b6f
Noys Alejandro Sanz <asanz91@gmail.com>
2013-12-20 17:09:44 +0100

    a little fix

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 $(document).ready(function() {
 
 
+
     var $myAudio = $('#epic-song');
     $myAudio.on('canplaythrough', function() {
         this.play();
     });
-    
+
     $('iframe').on('load', function() {
-    	
-    	var timeTransition = 20*1000,	// 20 seconds
-    		frequencyResizing = 100 // on miliseconds
-    		numIterations = timeTransition/frequencyResizing,
-    		initialWidth = $(window).height(),
-    		finalWidth = 320,
-    		differenceWidth = initialWidth - finalWidth,
-			step = differenceWidth / numIterations;
 
-    	$('iframe').height($(window).height());
+        var timeTransition = 20 * 1000, // 20 seconds
+            frequencyResizing = 100 // on miliseconds
+            numIterations = timeTransition / frequencyResizing,
+            initialWidth = $(window).height(),
+            finalWidth = 320,
+            differenceWidth = initialWidth - finalWidth,
+            step = differenceWidth / numIterations;
 
-    	$myAudio.trigger('canplaythrough');
-	    window.setInterval(function() {
-	    	if($('iframe').width() === finalWidth) return;
-	    	$('iframe').width($('iframe').width()-step);
-	    }, frequencyResizing);
+        $('iframe').height($(window).height());
+
+        $myAudio.trigger('canplaythrough');
+        window.setInterval(function() {
+            if ($('iframe').width() === finalWidth) return;
+            $('iframe').width($('iframe').width() - step);
+        }, frequencyResizing);
 
     });
 
