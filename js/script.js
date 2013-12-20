e4218a53186b0b5bf9480023eba772810764c1b9
Noys Alejandro Sanz <asanz91@gmail.com>
2013-12-20 16:24:50 +0100

    EPIC resize with easy customization of timing. Only width

--- js/script.js
+++ js/script.js
@@ -7,6 +7,23 @@ $(document).ready(function() {
     });
     
     $('iframe').on('load', function() {
+    	
+    	var timeTransition = 20*1000,	// 20 seconds
+    		frequencyResizing = 50 // on miliseconds
+    		numIterations = timeTransition/frequencyResizing,
+    		initialWidth = $(window).height(),
+    		finalWidth = 320,
+    		differenceWidth = initialWidth - finalWidth,
+			step = differenceWidth / numIterations;
+
+    	$('iframe').height($(window).height());
+
     	$myAudio.trigger('canplaythrough');
+	    window.setInterval(function() {
+	    	if($('iframe').width() === finalWidth) return;
+	    	$('iframe').width($('iframe').width()-step);
+	    }, frequencyResizing);
+
     });
+
 });
